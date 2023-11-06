f63c82866f5f76f3a8d364688371f37be91647c9
장경빈 <wkdrudqls114@gmail.com>
2023-11-06 14:35:03 +0900

    구매금액 입력 받기 & 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {}
+  
+  validateNumber(number){
+    const specialCharacters = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/;
+    const specialCharacter = specialCharacters.test(number);
+    const rightNumber = isNaN(number);
+    const space = number.includes(" ");
+    const thousandUnit = !(number%1000 === 0);
+    if(specialCharacter || rightNumber || space || thousandUnit) throw new Error("[ERROR] 숫자의 형식이 잘못돼었습니다.");
+  }
+  async play() {
+    const purchaseMoney = await Console.readLineAsync("구입 금액을 입력해 주세요.");
+    this.validateNumber(purchaseMoney);
+
+  }
 }
 
 export default App;
