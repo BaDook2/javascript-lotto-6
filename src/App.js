f0aa52f9c76015410c2d5e49a0708528dd6c46b5
장경빈 <wkdrudqls114@gmail.com>
2023-11-06 23:37:43 +0900

    당첨번호와 보너스 번호를 입력받은 후 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 
 class App {
   
   validateNumber(number){
     const specialCharacters = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/;
     const specialCharacter = specialCharacters.test(number);
-    const rightNumber = isNaN(number);
+    const isNumber = isNaN(number);
     const space = number.includes(" ");
     const thousandUnit = !(number%1000 === 0);
-    if(specialCharacter || rightNumber || space || thousandUnit) throw new Error("[ERROR] 숫자의 형식이 잘못돼었습니다.");
+    if(specialCharacter || isNumber || space || thousandUnit) throw new Error("[ERROR] 숫자의 형식이 잘못돼었습니다.");
   }
 
   makeLotto(lottoQuantity){
@@ -21,6 +22,7 @@ class App {
       Console.print(userLotto[count]);
     }
   }
+
   async play() {
     const purchaseMoney = await Console.readLineAsync("구입 금액을 입력해 주세요.\n");
 
@@ -28,6 +30,11 @@ class App {
     const lottoQuantity = purchaseMoney / 1000;
     Console.print("\n" + lottoQuantity + "개를 구매했습니다.");
     this.makeLotto(lottoQuantity);
+    let winningLotto = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+    winningLotto = winningLotto.split(',').join('');
+    let lotto = new Lotto(winningLotto);
+    let bonousNumber = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+    lotto.addBonousNumber(bonousNumber);
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,19 +1,33 @@
 class Lotto {
   #numbers;
-
+  
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
+  
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    const specialCharacters = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/;
+    const isNumbersSet = (new Set(numbers)).size !==6;
+    const isNumber = isNaN(numbers);
+    const hasSpecialCharacters = specialCharacters.test(numbers);
+    if(isNumbersSet || isNumber || hasSpecialCharacters) throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
 
-  // TODO: 추가 기능 구현
-  
+  #checkBonousNumber(bonousNumber){
+    const isNotOneNumber = bonousNumber.length !== 1;
+    const isOverlapNumber = this.#numbers.includes(bonousNumber);
+    const isNotANumber = isNaN(bonousNumber);
+    if(isNotOneNumber || isOverlapNumber || isNotANumber) throw new Error("[ERROR] 보너스 숫자가 잘못된 형식입니다.");
+  }
+
+  addBonousNumber(bonousNumber){
+    this.#checkBonousNumber(bonousNumber);
+    this.#numbers += this.bonousNumber;
+  }
+
+
 }
 
 export default Lotto;
